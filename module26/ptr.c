19cd19683719843161e2fc01139e01ad6fdd22a0
itisaraf <itisaraf@gmail.com>
2023-03-09 01:42:43 +0600

    added

--- module26/ptr.c
+++ module26/ptr.c
@@ -1,13 +1,1 @@
 
-#include<stdio.h>
-
-int main()
-{
-
-    int m, n ;
-    scanf("%d%d",&m,&n);
-    int *p=&m, *q=&n;
-    float k= (*p + *q);
-    printf("%.3f",k/2);
-    return 0;
-}
---
+++ module27/lab_assign27.c
@@ -0,0 +1,40 @@
+// Problem Statement
+// You are given a positive integer n .The second line will contain n positive integers.
+// Write a function that will take an integer array as an argument and return the sum in the following way.
+// >> If the index is odd and at the same time the value is odd then you can add the index and values
+// >> If the index is even and at the same time the value is even then you can add the index and values
+
+
+// Return the total sum from the function. If there is no such value which is mentioned in the above then return 0 from the function.
+// See the sample input ,output and explanation for more clarification. 
+
+// Constraints- 
+// 1<=n<=100
+// Values of array will be given between 1-1000
+
+
+
+#include<stdio.h>
+
+int main()
+{
+    // printf("Hello world!");
+    int i, n,sum =0;
+    scanf("%d", &n);
+    int arr[n];
+
+    for( i=1; i<n+1 ; i++)
+        scanf("%d",&arr[i]);              
+               
+    for( i=1; i<n+1 ; i++)
+    {
+        if((i%2==0)&&(arr[i]%2==0))
+            sum += arr[i]+ i;
+
+        else  if((i%2==1)&&(arr[i]%2==1))
+            sum += arr[i]+ i;
+    }
+
+    printf("%d",sum);
+    return 0;
+}
